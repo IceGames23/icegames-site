dc1ca430e275890632ee7b9912128ec531fec8d5
Vitor Albert <icegames2004@gmail.com>
2026-05-29 21:42:34 -0300

    feat: add BaseLayout with SEO meta and hreflang

---
+++ src/data/site.ts
@@ -0,0 +1,12 @@
+export const site = {
+  name: 'IceGames',
+  author: 'Vitor Albert',
+  url: 'https://icegames.dev', // placeholder
+  links: {
+    email: 'mailto:icegames2004@gmail.com',
+    github: 'https://github.com/', // TODO: real handle
+    linkedin: 'https://www.linkedin.com/', // TODO: real handle
+    discord: '#', // TODO: invite/handle
+  },
+  skills: ['Java', 'Spigot/Paper', 'Spring', 'Python', 'IA/LLMs', 'JavaScript', 'SQL', 'Git', 'Design'],
+};
